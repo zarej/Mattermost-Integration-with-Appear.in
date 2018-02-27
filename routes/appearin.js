053a13b11736985cacc20f8b8601658ad2b54bae
Zarko Jevtic <zarko.jevtic@appmodule.net>
2018-02-28 00:08:55 +0100

    Added button to join meeting

--- routes/appearin.js
+++ routes/appearin.js
@@ -5,13 +5,31 @@ const router = express.Router()
 
 router.post('/', (req, res) => {
   if (req.body.token !== process.env.TOKEN) {
-    res.status(401).json('Unauthorized')
+    return res.status(401).json('Unauthorized')
   }
   console.log('Request body:', req.body)
   const team = process.env.TEAM || req.body.team_domain
   const room = req.body.text || team + '_' + req.body.channel_name
-  const message = process.env.MESSAGE || 'Please click to join the call.'
-  res.json({'response_type': 'in_channel', 'text': '[' + message + '](http://appear.in/' + room + ')'})
+  let message = process.env.MESSAGE || 'Please click the button to join the meeting.'
+  let buttonTxt = process.env.BUTTON || 'Join the meeting'
+  req.body.room = room
+  Object.keys(req.body).forEach(key => {
+    message = message.replace(new RegExp('{{' + key + '}}', 'g'), req.body[key])
+    buttonTxt = buttonTxt.replace(new RegExp('{{' + key + '}}', 'g'), req.body[key])
+  })
+  const response = {
+    'response_type': 'in_channel',
+    'attachments': [{
+      'text': message,
+      'actions': [{
+        'name': buttonTxt,
+        'integration': {
+          'url': 'http://appear.in/' + room
+        }
+      }]
+    }]
+  }
+  res.json(response)
 })
 
 export default router
